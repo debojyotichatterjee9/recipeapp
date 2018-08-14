64f44d42166f6f6b63d3854dac33e956ef05cb48
debojyotichatterjee9 <debojyotichatterjee9@gmail.com>
2018-08-14 17:58:29 +0530

    created form component

---
+++ src/components/Form.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Form = () => (    //stateless functional component as we do not need a state here
+    <form>
+        <input type="text"/>
+        <button>Search</button>
+    </form>
+);
+
+export default Form;
