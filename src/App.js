977e9ef2e0ed39bc538074800b8fc9a08f563404
debojyotichatterjee9 <debojyotichatterjee9@gmail.com>
2018-08-14 22:18:00 +0530

    styles added

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Form from "./components/Form.js";
+import Recipes from "./components/Recipes.js";
 
 const API_KEY = "e0ad43edd0855699c2c3580f80c33561";
 
 
 class App extends Component {
+  state = {
+    recipes: [] //setting the state to an empty array
+  }
   getRecipe = async (e) =>{    //method responsible for making the API call
     const recipeName = e.target.elements.recipeName.value;
     e.preventDefault(); //this prevents the page to be refreshed on click
     //used https://cors-anywhere.herokuapp.com/ as the data cannot be fetched from a local host server
-    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken`);
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}`);
     const data = await api_call.json();
-    console.log(data);
-    console.log(recipeName);
+    this.setState({ recipes: data.recipes }); //setting the state after click
+    console.log(this.state.recipes);
+    //console.log(data.recipes[0].publisher);
+    //console.log(recipeName);
   }
   render() {
     return (
@@ -24,6 +30,8 @@ class App extends Component {
           <h1 className="App-title">Recipe Search</h1>
         </header>
         <Form getRecipe={this.getRecipe}/>
+        
+        <Recipes recipes={this.state.recipes} />
       </div>
     );
   }
